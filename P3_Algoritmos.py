0958d243a7956d15e76cddbdc5cec37c569068ce
Joshua <xyznathaniel@gmail.com>
2023-02-25 11:43:29 -0600

    Mostrar tabla

--- P3_Algoritmos.py
+++ P3_Algoritmos.py
@@ -12,3 +12,21 @@ class Proceso:
 		self.t_ret = 0
 		self.t_fin = 0
 
+	def get_tabla(self):
+		"""Imprime una tabla"""
+		tabla = f"|{self.nombre}|{self.t_lle}|{self.t_esp}|{self.t_res}|{self.t_eje}|{self.t_ret}|{self.t_fin}|"
+		return tabla
+
+#Lista de procesos
+cola = []
+
+def lista_procesos(filename):
+	"""Función para obtener la lista de procesos"""
+	with open(filename) as file_obj:
+		for procesos in file_obj:
+			valor = procesos.split(",")
+			cola.append(Proceso(valor[0], int(valor[1]), int(valor[2])))
+
+lista_procesos("procesos.txt")
+for obj in cola:
+	print(obj.get_tabla())
